da9da66a24889dcad77cd3608d4b1b00bd2feaa8
Delmondes <wellington.apk@gmail.com>
2025-01-08 00:23:03 -0300

    Correções para execução

--- p-program/code.asm
+++ p-program/code.asm
@@ -6,14 +6,21 @@ soma DD 00
 subtracao DD 00
 
 SECTION .text
-GLOBAL main
-main:
+GLOBAL _start
+
+_start:
+    ; Soma num1 + num2
     MOV EAX, [num1]
     MOV EBX, [num2]
-    MOV ECX, [num3]
-    ADD EAX, EBX ; SOMA +=
-    MOV [soma], EAX
-    SUB EAX, ECX ; SUBTRACAO 
-    MOV [subtracao], EAX
-    MOV EAX, 0
-    RET
+    ADD EAX, EBX
+    ADD [soma], EAX ; Armazena o resultado da soma em 'soma'
+
+    ; Subtração num1 - num3
+    MOV EAX, [num1] ; Recarrega num1 em EAX
+    SUB EAX, [num3]
+    ADD [subtracao], EAX ; Armazena o resultado da subtração em 'subtracao'
+
+    ; Finaliza o programa
+    MOV EAX, 1       ; Código de saída do sistema
+    MOV EBX, 0       ; Código de retorno
+    INT 0x80         ; Interrompe o sistema
