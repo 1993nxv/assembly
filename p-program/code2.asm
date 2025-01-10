bb01901b4fdcc7b0fab78818c6258d384c8969ac
Delmondes <wellington.apk@gmail.com>
2025-01-09 22:41:34 -0300

    Hello World

--- p-program/code2.asm
+++ p-program/code2.asm
@@ -1,6 +1,22 @@
+section .data
+    msg db 'Hello, World!', 0
+    title db 'Mensagem', 0
+
 section .text
-global main
+    global main
+    extern MessageBoxA, ExitProcess
+
 main:
-    ;write your code here
-    xor eax, eax
-    ret
+    ; Alinha a pilha a 16 bytes
+    sub rsp, 40       
+
+    ; Chama MessageBoxA(NULL, msg, title, MB_OK)
+    xor rcx, rcx      ; HWND = NULL
+    lea rdx, [msg]    ; LPCSTR lpText
+    lea r8, [title]   ; LPCSTR lpCaption
+    xor r9d, r9d      ; UINT uType = MB_OK
+    call MessageBoxA
+
+    ; Chama ExitProcess(0)
+    xor ecx, ecx      ; Código de saída
+    call ExitProcess
